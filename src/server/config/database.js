a9c15ec4b53d21f6836e9ef06f7fca9e877bdcd6
cisen <csacxc@gmail.com>
2019-09-23 22:02:28 +0800

    feat: 添加sqlite数据库到本地文件

--- src/server/config/database.js
+++ src/server/config/database.js
@@ -1,8 +1,10 @@
 import Sequelize from 'sequelize';
+import path from 'path';
 
 const sequelize = new Sequelize('graphql_sequelize_test', 'root', '', {
   host: 'localhost',
   dialect: 'sqlite',
+  storage: path.join(__dirname, 'database.sqlite')
 });
 
 export default sequelize;
